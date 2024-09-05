54dddce2ded14e5271927156244ef025c9bfcf15
Nora Kauczor <n.kauczor@web.de>
2024-09-05 10:26:54 +0200

    adjust old methods and tests to usage of OrderStatus

--- src/main/java/ShopService.java
+++ src/main/java/ShopService.java
@@ -1,23 +1,32 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 public class ShopService {
     private ProductRepo productRepo = new ProductRepo();
     private OrderRepo orderRepo = new OrderMapRepo();
+
+    // Write a method in the ShopService that returns a list of all orders with a specific order status (parameter) using streams.
+    public List<Order> getOrdersByStatus(OrderStatus requiredStatus) {
+        return orderRepo.getOrders().stream()
+                .filter(order -> order.status() == requiredStatus)
+                .collect(Collectors.toList());
+    }
+
 
     public Order addOrder(List<String> productIds) {
         List<Product> products = new ArrayList<>();
         for (String productId : productIds) {
             Product productToOrder = productRepo.getProductById(productId);
             if (productToOrder == null) {
-                System.out.println("Product mit der Id: " + productId + " konnte nicht bestellt werden!");
+                System.out.println("Product with ID " + productId + " could not be ordered.");
                 return null;
             }
             products.add(productToOrder);
         }
 
-        Order newOrder = new Order(UUID.randomUUID().toString(), products);
+        Order newOrder = new Order(UUID.randomUUID().toString(), products, OrderStatus.PROCESSING);
 
         return orderRepo.addOrder(newOrder);
     }
